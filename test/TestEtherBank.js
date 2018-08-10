cdb3547c1a1e23e65917fc8be608c0a8718c0be2
satyaP <sats.pasupuleti@gmail.com>
2018-08-10 16:07:44 -0400

    updated tests: Truffle.js now loads on ganache-cli in bash

--- test/TestEtherBank.js
+++ test/TestEtherBank.js
@@ -2,10 +2,14 @@ var EtherBank = artifacts.require("./EtherBank");
 contract("Etherbank", async(accounts)=>{
     it("should store 10 Ether from Ganache into account" , async() =>{
         let instance = await EtherBank.deployed();
+        //how 
+        var accountx = await web3.eth.accounts;
         
-       let balance = await instance.store.call(10,{from:accounts[0],gas:"220000"}); //account does not have any ether to transfer which is why the requires failed
-       assert.equal(balance.valueOf(),10);
-        
+        await console.log(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]).toNumber()));
+      let balance = await instance.store.call(10,{from:web3.eth.accounts[0],value: web3.toWei(50000000, 'ether')}); //account does not have any ether to transfer which is why the requires failed
+      // assert.equal(await balance.valueOf(),10);
+      await console.log(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]).toNumber()));
+      
 
     })
 });
