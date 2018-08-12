29be189c178ecca7e0ebc75dd2ecd74f3909f609
satyaP <sats.pasupuleti@gmail.com>
2018-08-12 17:54:21 -0400

    updated some comments/added current issues with testing

--- test/TestEtherBank.js
+++ test/TestEtherBank.js
@@ -7,11 +7,12 @@ contract("Etherbank", async(accounts)=>{
         
         await console.log(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]).toNumber()));
        await instance.store.call(10,{from:web3.eth.accounts[0],value: web3.toWei(10, 'ether')}); 
-      // assert.equal(await balance.valueOf(),10);
+       assert.equal(await balance.valueOf(),10);
       await console.log(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]).toNumber()));
-      //await console.log(web3.eth.getBalance(contract.options.address));
+      await console.log(web3.eth.getBalance(contract.options.address));
       
-      //displays contract balance
+      //displays contract balance using promises
+      //shows 0. No ether is being stored even though the account has test ether...
       var testBalance=instance.contractBalance();
       testBalance.then(function(result){
           console.log(web3.fromWei(result.toNumber(), "ether" ));
